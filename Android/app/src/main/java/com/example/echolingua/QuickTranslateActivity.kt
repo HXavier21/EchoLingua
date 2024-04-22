3491d864687c949826d72c5ec668b0345040e468
Xavier <hxavierhugo@gmail.com>
2024-04-22 21:33:46 +0800

    Android: build a blank quickTranslatePage

--- Android/app/src/main/java/com/example/echolingua/QuickTranslateActivity.kt
+++ Android/app/src/main/java/com/example/echolingua/QuickTranslateActivity.kt
@@ -1,8 +1,12 @@
-package com.example.echolingua
+package com.example.echoling
 
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -11,20 +15,28 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.echolingua.ui.page.QuickTranslatePage
+import com.example.echolingua.ui.page.TranslatePageViewModel
 import com.example.echolingua.ui.theme.EchoLinguaTheme
 
 class QuickTranslateActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        if (intent?.action == Intent.ACTION_PROCESS_TEXT || intent?.action == Intent.ACTION_SEND) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                val text = intent?.getCharSequenceExtra(Intent.EXTRA_PROCESS_TEXT)
+                if (text != null) {
+
+                }
+            }
+        }
+
+        enableEdgeToEdge()
         setContent {
             EchoLinguaTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = Color.Transparent
-                ) {
-                    Text(text = "ddsggfdfdfdfdfdfdf")
-                }
+                QuickTranslatePage()
             }
         }
 
