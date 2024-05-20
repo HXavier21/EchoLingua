3975d38dbc5e48fa70b5308960eca632d8321cf8
stranger156 <3426464117@qq.com>
2024-05-20 21:17:14 +0800

    change codes

--- Android/app/src/main/java/com/example/echolingua/ui/page/QuickTranslatePage.kt
+++ Android/app/src/main/java/com/example/echolingua/ui/page/QuickTranslatePage.kt
@@ -23,6 +23,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -35,7 +36,8 @@ import com.example.echolingua.R
 import com.example.echolingua.ui.theme.EchoLinguaTheme
 
 @Composable
-fun QuickTranslatePage() {
+fun QuickTranslatePage(state:Number) {
+    Column{
     ElevatedCard(
         elevation = CardDefaults.cardElevation(
             defaultElevation = 10.dp
@@ -44,105 +46,127 @@ fun QuickTranslatePage() {
             containerColor = MaterialTheme.colorScheme.onSurface,
         ),
         modifier = Modifier
+            .padding(10.dp)
             .clip(RoundedCornerShape(20.dp))
             .background(color = MaterialTheme.colorScheme.onSurface)
             .fillMaxWidth()
-            .padding(10.dp)
     ) {
         Row(
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(top = 10.dp, start = 10.dp)
         ) {
-            LeftIcon()
+            Icon(
+                painter = painterResource(id = R.drawable.translate),
+                contentDescription = null,
+                tint = Color.White,
+                modifier = Modifier
+                    .padding(start = 10.dp)
+                    .size(25.dp)
+            )
             Spacer(modifier = Modifier.weight(1f))
-            Title()
+            Text(
+                text = "EchoLingua翻译",
+                textAlign = TextAlign.Center,
+                color = Color.White,
+            )
             Spacer(modifier = Modifier.weight(1f))
-            RightIcon()
+            Icon(
+                imageVector = Icons.Filled.MoreVert,
+                contentDescription = null,
+                tint = Color.White,
+                modifier = Modifier
+                    .padding(start = 20.dp, end = 20.dp)
+                    .size(20.dp)
+            )
         }
 
         Row {
-            Tooltip()
+            Text(
+                text = "中文(简体)   - 检测到的语言",
+                color = Color.White,
+                modifier = Modifier
+                    .padding(top = 20.dp, start = 20.dp)
+            )
             Spacer(modifier = Modifier.weight(1f))
-            PasteIcon()
-            ReportIcon()
+            Icon(
+                painter = painterResource(id = R.drawable.paste),
+                contentDescription = null,
+                tint = Color.White,
+                modifier = Modifier
+                    .padding(end = 20.dp, top = 20.dp)
+                    .size(25.dp)
+            )
+            Icon(
+                painter = painterResource(id = R.drawable.report),
+                contentDescription = null,
+                tint = Color.White,
+                modifier = Modifier
+                    .padding(end = 20.dp, top = 20.dp)
+                    .size(25.dp)
+            )
         }
-        UndoWord()
+        Text(
+            text = "身体",
+            color = Color.White,
+            modifier = Modifier
+                .padding(top = 10.dp, start = 20.dp)
+        )
         Divider(
-            color = Color(0xFF424758),
+            color = Color.DarkGray,
             modifier = Modifier.padding(start = 100.dp, top = 20.dp, end = 100.dp)
         )
         Row {
-            Language()
+            Text(
+                text = "英语",
+                color = MaterialTheme.colorScheme.primaryContainer,
+                modifier = Modifier
+                    .padding(top = 20.dp, start = 20.dp)
+            )
             Spacer(modifier = Modifier.weight(1f))
-            PasteIconCopy()
-            ReportIconCopy()
+            Icon(
+                painter = painterResource(id = R.drawable.paste),
+                contentDescription = null,
+                tint = MaterialTheme.colorScheme.primaryContainer,
+                modifier = Modifier
+                    .padding(end = 20.dp, top = 20.dp)
+                    .size(25.dp)
+            )
+            Icon(
+                painter = painterResource(id = R.drawable.report),
+                contentDescription = null,
+                tint = MaterialTheme.colorScheme.primaryContainer,
+                modifier = Modifier
+                    .padding(end = 20.dp, top = 20.dp)
+                    .size(25.dp)
+            )
         }
-        DoneLanguage()
-    }
-}
-
-@Composable
-fun SecondElevatedCard() {
-    SecondElevatedCard()
-}
-
-@Composable
-fun Title() {
-    Column {
         Text(
-            text = "EchoLingua翻译",
-            textAlign = TextAlign.Center,
-            color = Color.White,
+            text = "human body",
+            color = MaterialTheme.colorScheme.primaryContainer,
+            modifier = Modifier
+                .padding(top = 10.dp, start = 20.dp, bottom = 20.dp)
         )
     }
-}
 
-@Composable
-fun Tooltip() {
-    Text(
-        text = "中文(简体)   - 检测到的语言",
-        color = Color.White,
+    Row(
         modifier = Modifier
-            .padding(top = 20.dp, start = 20.dp)
-    )
+            .fillMaxWidth()
+    ) {
+        ClearButton()
+        Spacer(modifier = Modifier.weight(1f))
+        NewTranslateButton()
+    }
 }
-
-@Composable
-fun Language() {
-    Text(
-        text = "英语",
-        color = Color(0xFFb6c5ff),
-        modifier = Modifier
-            .padding(top = 20.dp, start = 20.dp)
-    )
-}
-
-@Composable
-fun UndoWord() {
-    Text(
-        text = "身体",
-        color = Color.White,
-        modifier = Modifier
-            .padding(top = 10.dp, start = 20.dp)
-    )
-}
-
-@Composable
-fun DoneLanguage() {
-    Text(
-        text = "human body",
-        color = Color(0xFFb6c5ff),
-        modifier = Modifier
-            .padding(top = 10.dp, start = 20.dp, bottom = 20.dp)
-    )
 }
 
 @Composable
 fun ClearButton() {
     ElevatedButton(
-        onClick = { },
-        colors = ButtonDefaults.buttonColors(Color(0xFF414758)),
+        onClick = {
+
+        },
+        colors = ButtonDefaults.buttonColors(MaterialTheme.colorScheme.onSurfaceVariant),
         modifier = Modifier
             .padding(start = 10.dp)
     )
@@ -157,7 +181,7 @@ fun ClearButton() {
 fun NewTranslateButton() {
     ElevatedButton(
         onClick = { },
-        colors = ButtonDefaults.buttonColors(Color(0xFFb6c5ff)),
+        colors = ButtonDefaults.buttonColors(MaterialTheme.colorScheme.primaryContainer),
         modifier = Modifier
             .padding(end = 10.dp)
     )
@@ -169,17 +193,7 @@ fun NewTranslateButton() {
     }
 }
 
-@Composable
-fun LeftIcon() {
-    Icon(
-        painter = painterResource(id = R.drawable.translate),
-        contentDescription = null,
-        tint = Color.White,
-        modifier = Modifier
-            .padding(start = 10.dp)
-            .size(25.dp)
-    )
-}
+
 
 @Composable
 fun RightIcon() {
@@ -195,81 +209,10 @@ fun RightIcon() {
 
 }
 
-@Composable
-fun PasteIcon() {
-    Icon(
-        painter = painterResource(id = R.drawable.paste),
-        contentDescription = null,
-        tint = Color.White,
-        modifier = Modifier
-            .padding(end = 20.dp, top = 20.dp)
-            .size(25.dp)
-    )
 
-}
-
-@Composable
-fun PasteIconCopy() {
-    Icon(
-        painter = painterResource(id = R.drawable.paste),
-        contentDescription = null,
-        tint = Color(0xFFb6c5ff),
-        modifier = Modifier
-            .padding(end = 20.dp, top = 20.dp)
-            .size(25.dp)
-    )
-
-}
-
-@Composable
-fun ReportIcon() {
-    Icon(
-        painter = painterResource(id = R.drawable.report),
-        contentDescription = null,
-        tint = Color.White,
-        modifier = Modifier
-            .padding(end = 20.dp, top = 20.dp)
-            .size(25.dp)
-    )
-
-}
-
-@Composable
-fun ReportIconCopy() {
-    Icon(
-        painter = painterResource(id = R.drawable.report),
-        contentDescription = null,
-        tint = Color(0xFFb6c5ff),
-        modifier = Modifier
-            .padding(end = 20.dp, top = 20.dp)
-            .size(25.dp)
-    )
-
-}
-
-
-@Preview(showBackground = true)
-@Composable
-fun TitlePreview() {
-    EchoLinguaTheme {
-        Column {
-            QuickTranslatePage()
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                ) {
-                    ClearButton()
-                    Spacer(modifier = Modifier.weight(1f))
-                    NewTranslateButton()
-                }
-
-        }
-
-    }
-}
 @Composable
 fun ElevatedCard() {
-    androidx.compose.material3.ElevatedCard(
+    ElevatedCard(
         elevation = CardDefaults.cardElevation(
             defaultElevation = 10.dp
         ),
@@ -286,13 +229,29 @@ fun ElevatedCard() {
             .fillMaxWidth()
             .padding(top = 10.dp, start = 10.dp)
         ){
-            LeftIcon()
+            Icon(
+                painter = painterResource(id = R.drawable.translate),
+                contentDescription = null,
+                tint = Color.White,
+                modifier = Modifier
+                    .padding(start = 10.dp)
+                    .size(25.dp)
+            )
             Spacer(modifier = Modifier.weight(1f))
-            Title()
+            Text(
+                text = "EchoLingua翻译",
+                textAlign = TextAlign.Center,
+                color = Color.White,
+            )
             Spacer(modifier = Modifier.weight(1f))
             RightIcon()
         }
-        Tooltip()
+        Text(
+            text = "中文(简体)   - 检测到的语言",
+            color = Color.White,
+            modifier = Modifier
+                .padding(top = 20.dp, start = 20.dp)
+        )
         InputBox()
         PasteButton()
     }
@@ -315,35 +274,16 @@ fun InputBox() {
         }
     )
 }
-
 @Composable
-fun Title(
-    modifier: Modifier = Modifier) {
-    Column {
-        Text(
-            text = "EchoLingua翻译",
-            color= Color.White,
-            modifier = modifier
-        )
-    }
+fun StateChoose(){
+    var state by rememberSaveable { mutableStateOf(1) }
+    QuickTranslatePage(state=state)
 }
-@Composable
-fun Tooltip( modifier: Modifier = Modifier) {
-    Column {
-        Text(
-            text = "检测语言",
-            color= Color.White,
-            modifier = Modifier
-                .padding(start = 20.dp, top = 20.dp)
-        )
-    }
-}
-
 @Composable
 fun PasteButton() {
     ElevatedButton(
         onClick = {  },
-        colors = ButtonDefaults.buttonColors(Color(0xFF414758)),
+        colors = ButtonDefaults.buttonColors(MaterialTheme.colorScheme.onSurfaceVariant),
         modifier = Modifier
             .padding(start = 20.dp, top = 20.dp, bottom = 20.dp)
     )
@@ -360,10 +300,11 @@ fun PasteButton() {
 fun SecondTitlePreview() {
     EchoLinguaTheme {
         Column {
-            ElevatedCard()
+            StateChoose()
         }
 
     }
 }
+
 
 
